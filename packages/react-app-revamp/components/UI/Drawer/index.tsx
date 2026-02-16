f180b4ef331f5692d0f994755703d5c02b8b3265
nakedfool <medenicablazo@gmail.com>
2026-02-16 13:46:31 +0100

    bugfix: fix mobile voting drawer

--- packages/react-app-revamp/components/UI/Drawer/index.tsx
+++ packages/react-app-revamp/components/UI/Drawer/index.tsx
@@ -17,19 +17,20 @@ const Drawer: FC<DrawerProps> = ({ isOpen, children, className, onClose, isHandl
   };
 
   return (
-    <VaulDrawer.Root open={isOpen} onOpenChange={handleOpenChange}>
+    <VaulDrawer.Root open={isOpen} onOpenChange={handleOpenChange} repositionInputs={false} handleOnly>
       <VaulDrawer.Portal>
         <VaulDrawer.Overlay className="fixed inset-0 bg-neutral-8/40 z-40" />
         <VaulDrawer.Content
           title="Drawer"
-          className={`z-50 rounded-t-[40px] border-t border-l border-r border-neutral-17 h-fit fixed bottom-0 left-0 right-0 outline-none ${className}`}
+          className={`z-50 rounded-t-[40px] border-t border-l border-r border-neutral-17 fixed bottom-0 left-0 right-0 outline-none flex flex-col ${className}`}
+          style={{ maxHeight: "calc(100dvh - 48px)" }}
         >
           <VaulDrawer.Title hidden>Drawer</VaulDrawer.Title>
           <VaulDrawer.Handle
             hidden={isHandleHidden}
-            className="!mx-auto !w-12 !h-1.5 !flex-shrink-0 !rounded-full !bg-neutral-9 !my-4"
+            className="mx-auto! w-12! h-1.5! shrink-0! rounded-full! bg-neutral-9! my-4!"
           />
-          {children}
+          <div className="flex-1 overflow-y-auto overflow-x-hidden overscroll-contain">{children}</div>
         </VaulDrawer.Content>
       </VaulDrawer.Portal>
     </VaulDrawer.Root>
