45d5d5fd460c42ffb87d1d7cb6ba6d50ae83afd6
zho <medenicablazo@gmail.com>
2026-03-10 14:08:58 +0100

    feat: add onramping with para (#5264)
    * feat: add onramping with para
    
    * feat: rm providers and add para as main one
    
    * feat: avoid complex ux
    
    * feat: fix duplicate providers (could be due to merge)
    
    * feat: add diff chevron based on modal variant
    
    * feat: update language
    
    * feat: adapt diff language
    
    * feat: change language
    
    ---------
    
    Co-authored-by: Siobhán McCaffery <smccaffery9@proton.me>

--- packages/react-app-revamp/components/AddFunds/index.tsx
+++ packages/react-app-revamp/components/AddFunds/index.tsx
@@ -2,8 +2,8 @@ import { getChainLogo } from "@helpers/getChainLogo";
 import Image from "next/image";
 import { FC } from "react";
 import { useShallow } from "zustand/shallow";
-import AddFundsProviders from "./providers";
 import AddFundsToggle from "./components/Toggle";
+import AddFundsProviders from "./providers";
 import { useAddFundsStore } from "./store";
 
 interface AddFundsProps {
@@ -12,9 +12,10 @@ interface AddFundsProps {
   showBackButton?: boolean;
   className?: string;
   onGoBack?: () => void;
+  onCloseModal?: () => void;
 }
 
-const AddFunds: FC<AddFundsProps> = ({ chain, asset, onGoBack, showBackButton = true, className }) => {
+const AddFunds: FC<AddFundsProps> = ({ chain, asset, onGoBack, showBackButton = true, className, onCloseModal }) => {
   const chainLogo = getChainLogo(chain);
   const { providerType, setProviderType } = useAddFundsStore(
     useShallow(state => ({
@@ -26,20 +27,22 @@ const AddFunds: FC<AddFundsProps> = ({ chain, asset, onGoBack, showBackButton = 
   return (
     <div className={`flex flex-col w-full h-full ${className}`}>
       <div className="flex flex-col gap-4 md:gap-6 flex-1 min-h-0">
-        <div className="flex flex-col gap-6">
-          <div className="flex items-start md:items-center justify-between w-full">
+        <div className="flex items-start md:items-center justify-between w-full">
+          <div className="flex items-center gap-3">
+            <p className="text-[24px] font-bold text-neutral-11">
+              add funds <span className="text-[12px]">on </span>
+            </p>
             <div className="flex items-center gap-3">
-              <p className="text-[24px] font-bold text-neutral-11">
-                add funds <span className="text-[12px]">on </span>
-              </p>
-              <div className="flex items-center gap-3">
-                <Image src={chainLogo} alt={chain} width={32} height={32} />
-                <p className="text-[24px] font-normal">{chain}</p>
-              </div>
+              <Image src={chainLogo} alt={chain} width={32} height={32} />
+              <p className="text-[24px] font-normal">{chain}</p>
             </div>
           </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
           <AddFundsToggle value={providerType} onChange={setProviderType} />
-          <AddFundsProviders chain={chain} asset={asset} type={providerType} />
+
+          <AddFundsProviders chain={chain} asset={asset} type={providerType} onCloseModal={onCloseModal} />
         </div>
       </div>
       {showBackButton && (
