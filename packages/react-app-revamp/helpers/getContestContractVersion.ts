593dd439a8f8119fbb0d4bdf09d49c19b22b25af
Siobhán McCaffery <smccaffery9@proton.me>
2026-03-11 10:36:14 -0400

    chore: bump version and update bytecode

--- packages/react-app-revamp/helpers/getContestContractVersion.ts
+++ packages/react-app-revamp/helpers/getContestContractVersion.ts
@@ -65,6 +65,7 @@ import UpdateFoundryContract from "@contracts/bytecodeAndAbi/Contest.6.13.update
 import RemoveUnusedMetadatasContract from "@contracts/bytecodeAndAbi/Contest.6.14.removeUnusedMetadatas.sol/Contest.json";
 import DeprecateSortingEnabledContract from "@contracts/bytecodeAndAbi/Contest.6.15.deprecateSortingEnabled.sol/Contest.json";
 import AddAnalyticsContract from "@contracts/bytecodeAndAbi/Contest.6.16.addAnalytics.sol/Contest.json";
+import CompleteCostToEnterDepContract from "@contracts/bytecodeAndAbi/Contest.6.16.addAnalytics.sol/Contest.json";
 import DeployedContestContract from "@contracts/bytecodeAndAbi/Contest.sol/Contest.json";
 import { MAX_TIME_TO_WAIT_FOR_RPC, executeWithTimeout } from "./timeout";
 import { createTransport } from "@config/wagmi/chains";
@@ -77,6 +78,7 @@ type ContractData = {
 };
 
 const VERSION_TO_CONTRACT: Record<string, ContractData> = {
+  "6.17": CompleteCostToEnterDepContract,
   "6.16": AddAnalyticsContract,
   "6.15": DeprecateSortingEnabledContract,
   "6.14": RemoveUnusedMetadatasContract,
