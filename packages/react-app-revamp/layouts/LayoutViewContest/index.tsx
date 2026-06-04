43e885fb92a2551feb5e2c154bdb4bea35d15102
nakedfool <medenicablazo@gmail.com>
2026-06-04 08:42:19 +0200

    bugfix: fix cut-off entry image in voting sidebar

--- packages/react-app-revamp/layouts/LayoutViewContest/index.tsx
+++ packages/react-app-revamp/layouts/LayoutViewContest/index.tsx
@@ -122,7 +122,7 @@ const LayoutViewContest = () => {
           </div>
         </div>
         {showSidebar && (
-          <aside className="hidden xl:block xl:w-[480px] xl:shrink-0 xl:mt-4 sticky top-4 self-start max-h-[calc(100vh-2rem)] overflow-y-auto sidebar-scroll">
+          <aside className="hidden xl:block xl:w-[480px] xl:shrink-0 xl:mt-4 sticky top-4 self-start max-h-[calc(100vh-2rem)] overflow-y-auto no-scrollbar">
             <VotingSidebar />
           </aside>
         )}
