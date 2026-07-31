930c64a3d04546002b35faead869b89502ae2c94
nakedfool <medenicablazo@gmail.com>
2026-07-31 12:54:25 +0200

    bugfix: fix deployment stalling on polygon

---
+++ packages/react-app-revamp/hooks/useDeployContest/deployment/rewards/constants.ts
@@ -0,0 +1,7 @@
+import { polygon } from "viem/chains";
+
+export const WALLET_RPC_SYNC_CHAIN_IDS = new Set<number>([polygon.id]);
+export const WALLET_RPC_SYNC_DELAY_MS = 4000;
+
+export const MAX_ATTACH_ATTEMPTS = 5;
+export const ATTACH_RETRY_DELAY_MS = 3000;
