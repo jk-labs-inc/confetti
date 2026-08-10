27411744c9eefb0981ec42a3218b3660d8fb7f43
nakedfool <medenicablazo@gmail.com>
2026-08-10 13:36:47 +0200

    bugfix: fix rewards module deploy auto failing

--- packages/react-app-revamp/hooks/useDeployContest/deployment/rewards/constants.ts
+++ packages/react-app-revamp/hooks/useDeployContest/deployment/rewards/constants.ts
@@ -5,3 +5,6 @@ export const WALLET_RPC_SYNC_DELAY_MS = 4000;
 
 export const MAX_ATTACH_ATTEMPTS = 5;
 export const ATTACH_RETRY_DELAY_MS = 3000;
+
+export const MAX_DEPLOY_ATTEMPTS = 5;
+export const DEPLOY_RETRY_DELAY_MS = 3000;
