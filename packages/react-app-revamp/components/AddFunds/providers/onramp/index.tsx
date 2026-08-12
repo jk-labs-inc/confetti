61a1f7e12f5c92c964f86bab4e37d38a75300b21
nakedfool <medenicablazo@gmail.com>
2026-08-12 15:07:18 +0200

    enhancement: fix onramp state when wallet is connected to a diff chain

--- packages/react-app-revamp/components/AddFunds/providers/onramp/index.tsx
+++ packages/react-app-revamp/components/AddFunds/providers/onramp/index.tsx
@@ -1,4 +1,9 @@
+import { toastError } from "@components/UI/Toast";
+import { getWagmiConfig } from "@getpara/evm-wallet-connectors";
 import { useModal } from "@getpara/react-sdk-lite";
+import { getChainId } from "@helpers/getChainId";
+import { useWallet } from "@hooks/useWallet";
+import { switchChain } from "@wagmi/core";
 import { FC } from "react";
 import AddFundsCard from "../../components/Card";
 import { PARA_ONRAMP_CONFIG } from "./types";
@@ -10,8 +15,19 @@ interface AddFundsParaProviderProps {
 
 const AddFundsParaProvider: FC<AddFundsParaProviderProps> = ({ chain, onCloseModal }) => {
   const { openModal } = useModal();
+  const { chain: connectedChain } = useWallet();
+  const contestChainId = getChainId(chain);
 
-  const handleClick = () => {
+  const handleClick = async () => {
+    if (connectedChain?.id !== contestChainId) {
+      try {
+        await switchChain(getWagmiConfig(), { chainId: contestChainId });
+      } catch {
+        toastError({ message: `Please switch your wallet to ${chain} to add funds` });
+        return;
+      }
+    }
+
     onCloseModal?.();
     openModal({ step: "ACCOUNT_ADD_FUNDS_BUY" });
   };
